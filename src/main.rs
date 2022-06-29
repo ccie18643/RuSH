efc0b875cc8312dabf298f52cf721575c79ad77c
Sebastian Majewski <ccie18643@gmail.com>
2022-06-29 07:35:41 -0500

    Working on adding proper support for command groups

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,46 @@ use std::io;
 use std::io::Write;
 use std::process;
 
-fn parse_command(command: String) -> (String, Vec<String>) {
-    let mut tokens = command.split_whitespace();
-    let cmd = match tokens.next() {
-        Some(cmd) => cmd.to_string(),
-        None => String::new(),
-    };
-    let args = tokens.map(|t| t.to_string()).collect();
-    (cmd, args)
-}
-
 fn print_command_prompt() {
     let current_dir = env::current_dir().unwrap();
-    print!("[{}]$ ", current_dir.display());
+    print!("<RuSH> [{}]$ ", current_dir.display());
     io::stdout().flush().unwrap();
 }
 
-fn execute_external_command(cmd: String, args: Vec<String>) {
-    let mut child = process::Command::new(cmd).args(args).spawn().unwrap();
+fn parse_command(cmd: &str) -> (&str, Vec<&str>) {
+    let mut tokens = cmd.split_whitespace();
+    (tokens.next().unwrap_or(""), tokens.collect())
+}
+
+fn execute_command(cmd_str: &str, cmd_args: Vec<&str>) {
+    let mut child = match process::Command::new(cmd_str).args(cmd_args).spawn() {
+        Ok(child) => child,
+        Err(_) => {
+            println!("-rush: {}: command not found", cmd_str);
+            return;
+        }
+    };
     child.wait().unwrap();
 }
 
-fn main() {
-    println!("\n*** Entering RuSH world *** \n");
+fn parse_command_group(cmd_grp: &str) -> Vec<(&str, Vec<&str>)> {
+    let mut cmds = Vec::new();
+    for token in cmd_grp.split('|') {
+        cmds.push(parse_command(token));
+    }
+    cmds
+}
 
+fn execute_command_group(cmds: Vec<(&str, Vec<&str>)>) {
+    for (cmd_str, cmd_args) in cmds {
+        match cmd_str {
+            "exit" => process::exit(0),
+            _ => execute_command(cmd_str, cmd_args),
+        }
+    }
+}
+
+fn main() {
     loop {
         print_command_prompt();
         let mut input = String::new();
@@ -35,13 +51,7 @@ fn main() {
             continue;
         }
 
-        let (cmd, args) = parse_command(input);
-
-        match cmd.as_str() {
-            "exit" => break,
-            _ => execute_external_command(cmd, args),
-        }
+        let cmds = parse_command_group(&input);
+        execute_command_group(cmds);
     }
-
-    println!("\n*** Exiting RuSH world *** \n");
 }
